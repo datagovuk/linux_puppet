a7cf3c6c330146de014c01cc39aa6a6e149ad5d7
Noel Sharpe <noels@radnetwork.co.uk>
2014-03-19 17:29:37 +0000

    Don't bother with adding pub key as keys are now published.

--- bin/vagrant-provision.sh
+++ bin/vagrant-provision.sh
@@ -17,7 +17,6 @@ then
     echo "    User vagrant_ro" >> /home/co/.ssh/config
     cp /vagrant/keys/id_rsa  /home/co/.ssh/id_rsa
     cp /vagrant/keys/id_rsa.pub  /home/co/.ssh/id_rsa.pub
-    cat /vagrant/keys/id_rsa.pub >> /home/co/.ssh/authorized_keys
     chown co: /home/co/.ssh/id_rsa.pub
     chown co: /home/co/.ssh/id_rsa
     chmod 600 /home/co/.ssh/id_rsa.pub /home/co/.ssh/id_rsa
