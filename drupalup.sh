907d3fec0ef269065830d29bf372c973a882092f
Jason Brown <jason@silex-consulting.com>
2014-04-11 16:42:02 +0100

    Add drush composer-manager to drupalup.sh to rebuild .json file

--- drupalup.sh
+++ drupalup.sh
@@ -4,6 +4,7 @@ sudo rm -rf dgu_d7
 git clone https://github.com/datagovuk/dgu_d7.git
 cd dgu_d7
 sudo rm -rf /home/co/dgud7
+sudo cp /home/co/dgud7/profiles/dgu/sample/php.ini /etc/php5/apache2/php.ini
 drush make --working-copy --no-gitinfofile distro.make /home/co/dgud7
 cd /home/co/dgud7
 sudo rm -rf /home/co/dgud7/shared_dguk_assets
@@ -16,7 +17,12 @@ ln -s /home/co/dgud7/shared_dguk_assets/assets
 drush  --db-url=mysql://root:dev@localhost/dgud7 --uri=dgud7 --root=/home/co/dgud7 --yes --verbose site-install dgu --sites-subdir=default --account-name=admin --account-pass=admin --site-mail='dgud7-info@vagrant.dgudev' --site-name='dgud7' --yes
 sudo rm -rf /var/www/dgud7
 sudo ln -sf /home/co/dgud7 /var/www/
+sudo chmod -R /home/co/dgud7/sites/default/files/
 cp /home/co/dgud7/profiles/dgu/sample/images/*.jpg /home/co/dgud7/sites/default/files/
 gunzip /home/co/dgud7/profiles/dgu/sample/dgud7_default_db.sql.gz
 mysql -u root -pdev dgud7 < /home/co/dgud7/profiles/dgu/sample/dgud7_default_db.sql
+drush cc all
+drush updatedb
+drush --yes composer-json-rebuild
+drush --yes composer-manager
 echo "You should now be able to browse to http://localhost:8000"
