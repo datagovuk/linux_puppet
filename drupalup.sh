4f74255162704bdc7205baa36a24f1f2e3d8ac5f
Jason Brown <jason@silex-consulting.com>
2014-04-11 13:34:06 +0100

    correct drupalup.sh file path parameter

--- drupalup.sh
+++ drupalup.sh
@@ -16,7 +16,7 @@ ln -s /home/co/dgud7/shared_dguk_assets/assets
 drush  --db-url=mysql://root:dev@localhost/dgud7 --uri=dgud7 --root=/home/co/dgud7 --yes --verbose site-install dgu --sites-subdir=default --account-name=admin --account-pass=admin --site-mail='dgud7-info@vagrant.dgudev' --site-name='dgud7' --yes
 sudo rm -rf /var/www/dgud7
 sudo ln -sf /home/co/dgud7 /var/www/
-cp /home/co/dgud7/sample/images/*.jpg /home/co/dgud7/sites/default/files/
-gunzip /home/co/dgud7/sample/dgud7_default_db.sql.gz
+cp /home/co/dgud7/profiles/dgu/sample/images/*.jpg /home/co/dgud7/sites/default/files/
+gunzip /home/co/dgud7/profiles/dgu/sample/dgud7_default_db.sql.gz
 mysql -u root -pdev dgud7 < /home/co/dgud7/sample/dgud7_default_db.sql
 echo "You should now be able to browse to http://localhost:8000"
