63c393c0d120891cff9f680c47f9aefae0477f38
Noel Sharpe <noels@radnetwork.co.uk>
2015-02-02 15:44:03 +0000

    Install puppetDB and deps.

--- bin/vagrant-provision.sh
+++ bin/vagrant-provision.sh
@@ -24,16 +24,28 @@ then
     echo dev > /etc/role
 
     #Temporary - need to add these to the base box?
-    apt-get update
+    wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+    sudo dpkg -i puppetlabs-release-precise.deb
+    sudo apt-get update
+    sudo puppet resource package puppetdb-terminus ensure=latest
+    sudo puppet resource package puppetdb ensure=latest
+
     sudo gem update
     sudo gem install hiera
     sudo gem install hiera-eyaml
+    #install puppetdb terminus
+
     rm -rf /etc/puppet
     ln -sf /vagrant/puppet /etc/
     ln -sf /etc/puppet/hiera.yaml /etc/
     #Update puppet module dependencies using librarian-puppet
-    cd /vagrant/puppet
-    librarian-puppet update
+    #cd /vagrant/puppet
+    #librarian-puppet update
+
+    mkdir /etc/puppet_ssl
+    ln -sf /etc/puppet_ssl /etc/puppet/ssl
+    puppet apply --test
+
 
     if [ ! -f /vagrant/keys/private_key.pkcs7.pem ];
     then
@@ -51,5 +63,6 @@ sudo /usr/bin/puppet apply --verbose \
   --hiera_config=${puppet_base}/hiera.yaml \
   ${to_provision}
 
+#sudo /usr/bin/puppet apply --verbose --manifestdir=/vagrant/puppet/manifests/ --modulepath=/vagrant/puppet/modules/ --hiera_config=/vagrant/puppet/hiera.yaml /vagrant/puppet/manifests/site.pp
 
 #su co -c 'ssh -o StrictHostKeyChecking=no dev.data.gov.uk "echo Connection success" < /dev/null'
