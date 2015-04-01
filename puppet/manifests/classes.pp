e3135633da6a7f7935029c0cc46eb1cd94de85e8
Noel Sharpe <noels@radnetwork.co.uk>
2015-04-01 16:07:59 +0100

    Add mod_php to apache.

--- puppet/manifests/classes.pp
+++ puppet/manifests/classes.pp
@@ -267,6 +267,7 @@ class dgu_organogram_explorer {
     group => 'www-data',
     mode => 775,
   }
+  apache::mod::php
   apache::vhost { 'organogram.data.gov.uk':
     override      => 'All',
     port          => 80,
